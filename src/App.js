4ea8775f5e12881e6bbf248261633d19e09e9406
Simulation42 <67242768+Simulation42@users.noreply.github.com>
2021-12-30 01:33:54 -0500

    stories update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import fb from './facebook.png'
 import lk from './linkedin.png'
 import gh from './github.png'
 import val from './valhalla.jpg';
+import nar from './naruto.png'
+import dog from './doge.jpg'
+import gpu from './gpu.jpg'
 
 function App() {
   return (
     <div className="outerBox">
       <div className="navBar">
         <div className="navBrand">
-          <span>ALEXANDER</span><span style={{ color: "red", fontWeight: "600", fontFamily: "Arial" }}>C++</span>
+          <span>ALEXANDER</span><span style={{ color: "rgb(70,80,130)", fontWeight: "600", fontFamily: "Arial" }}>C++</span>
         </div>
         <div className="linkBox">
           <a rel="noreferrer" title="Facebook" href="https://www.facebook.com/alex.cho.90/" target="_blank">
@@ -84,15 +87,39 @@ function App() {
           </div>
         </div>
         <div className="box">
-        <img className="boxImg" src={val} alt="none"></img>
+        <img className="boxImg" src={nar} alt="none"></img>
             <div className="boxHead">
-              Game Review++: Assassin's Creed Valhalla
+              Anime Review++: Naruto
             </div>
             <div className="boxText">
-              An indepth review of the latest Assassin's Creed game with a ++ take
+              Is this an overhyped anime that just came at the right time or a timeless++ classic?  
             </div>
             <div className="boxFoot">
               <span style={{textDecoration: "underline"}}>REVIEW</span> | Click to Read
+            </div>
+        </div>
+        <div className="box">
+        <img className="boxImg" src={gpu} alt="none"></img>
+            <div className="boxHead">
+              Top 10 GPUs That You Can't Buy
+            </div>
+            <div className="boxText">
+              These are the best++ GPUs currently avaiable that is not currently available
+            </div>
+            <div className="boxFoot">
+              <span style={{textDecoration: "underline"}}>PRODUCT</span> | Click to Read
+            </div>
+        </div>
+        <div className="box">
+        <img className="boxImg" src={dog} alt="none"></img>
+            <div className="boxHead">
+              Thoughts++: DogeCoin
+            </div>
+            <div className="boxText">
+              Is Dogecoin a scam or a brilliant creation that will take mankind to the future?
+            </div>
+            <div className="boxFoot">
+              <span style={{textDecoration: "underline"}}>THOUGHTS</span> | Click to Read
             </div>
         </div>
         <div className="box">
